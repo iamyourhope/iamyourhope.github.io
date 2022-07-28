dcffaa18cf01e2a49b22e417fdffc826b8c62015
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-07-29 01:18:40 +0600

    add loading screens

---
+++ AR/scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let scene = document.querySelector('a-scene');
+    let splash = document.querySelector('.arjs-loader');
+    scene.addEventListener('loaded', (e) => {
+        splash.style.display = 'none';
+    });
+});
